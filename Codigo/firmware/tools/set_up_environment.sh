b96b5bdec52f0ad9ec7cfe0d218b424132b55400
Matias <mfvazquez@fi.uba.ar>
2020-07-18 18:56:51 -0300

    Installing a virtual environment with the python needs in the set up script

--- Codigo/firmware/tools/set_up_environment.sh
+++ Codigo/firmware/tools/set_up_environment.sh
@@ -1,5 +1,8 @@
-echo "Please enter the sudo password.\n\bPassword:" 
-stty -echo
-read SUDO_PASSWORD
-stty echo
-echo ${SUDO_PASSWORD} | sudo -S apt -y install $(cat $(dirname $0)/$package_file)
+sudo -S apt -y install $(cat packages.ubuntu.18.04.txt)
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+echo "New virtualenvironment configured. Can be activated with the command:"
+echo ""
+echo "		source venv/bin/activate"
+echo ""
